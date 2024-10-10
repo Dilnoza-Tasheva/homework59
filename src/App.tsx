69494366dbedb73ed029f710287477ce37182e22
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-10 12:29:36 +0200

    Ex 1 is DONE. Added second exercise to the App.tsx + a button to switch between the two exercises

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Movies from './Containers/Movies/Movies.tsx';
+import { useState } from 'react';
+import Jokes from './Containers/Jokes/Jokes.tsx';
 
 const App = () => {
+  const [page, setPage] = useState<boolean>(false)
 
 
   return (
     <>
-      <Movies/>
+      <button onClick={() => setPage(!page)} className="btn btn-success">Change the exercise</button>
+      {page ? <Movies/> : <Jokes/>}
     </>
   )
 };
