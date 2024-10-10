a0022f12fa1ec6fe048010fe57c8c271942f8847
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-10 12:59:05 +0200

    Added components to the jokes container, used async fetch function to gwt the joke from API. Added function to the useEffect to ensure that jokes are fetched only once.

--- src/Containers/Jokes/Jokes.tsx
+++ src/Containers/Jokes/Jokes.tsx
@@ -1,10 +1,32 @@
 import Joke from '../../Components/Joke/Joke.tsx';
+import GetNewJokeBtn from '../../Components/GetNewJokeBtn/GetNewJokeBtn.tsx';
+import { useEffect, useState } from 'react';
 
 
 const Jokes = () => {
+  const [joke, setJokes] = useState("");
+  const url = 'https://api.chucknorris.io/jokes/random';
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (response.ok)  {
+        const data = await response.json();
+        setJokes(data.value);
+      }
+    } catch (error) {
+      console.log('Error fetching the joke', error)
+    }
+  };
+
+  useEffect(() => {
+    void fetchData();
+  }, [])
+
   return (
     <div>
       <Joke joke={joke}/>
+      <GetNewJokeBtn addNewJoke={fetchData}/>
     </div>
   );
 };
